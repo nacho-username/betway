b8c8a384067b399730aedddafd56d6ab7f26dab3
Murray Williams <murray@waterfalldigital.co.za>
2023-04-19 20:45:23 +0200

    Adds close modal and minor style adjustments

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { useState } from 'react'
 
 interface ModalProps {
+  handleCloseModal: () => void
   isModalOpen: boolean
 }
 
@@ -9,7 +10,7 @@ interface FormValues {
   [key: string]: string
 }
 
-const Modal: React.FC<ModalProps> = ({ isModalOpen }) => {
+const Modal: React.FC<ModalProps> = ({ isModalOpen, handleCloseModal }) => {
   const [activeForm, setActiveForm] = useState('login')
   const [formValues, setFormValues] = useState<FormValues>({
     email: '',
@@ -32,12 +33,63 @@ const Modal: React.FC<ModalProps> = ({ isModalOpen }) => {
         isModalOpen ? 'block' : 'hidden'
       }`}
     >
-      <div className='bg-white rounded-lg p-8 w-full max-w-lg'>
-        <h2 className='text-2xl font-bold mb-4'>Login</h2>
-        <p className='mb-4 text-lg font-bold'>
-          New customer
+      <div className='relative bg-white p-8 w-full max-w-lg'>
+        <button
+          className='absolute top-0 right-2 p-2'
+          onClick={handleCloseModal}
+        >
+          <span className='text-lg'>X</span>
+        </button>
+        <h2 className='text-2xl font-bold text-center'>Login</h2>
+        <p className='mb-4 text-sm font-light text-center'>
+          New customer{' '}
           <span className='underline text-betway-green'>Register here</span>
         </p>
+        <form onSubmit={handleFormSubmit}>
+          <div className='mb-4'>
+            <label htmlFor='email' className='block text-gray-700 text-sm mb-2'>
+              Username
+            </label>
+            <input
+              type='email'
+              name='email'
+              id='email'
+              placeholder='Username'
+              value={formValues.email}
+              onChange={handleInputChange}
+              className='border rounded w-full py-2 px-3 text-gray-700'
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='password'
+              className='block text-gray-700 text-sm mb-2'
+            >
+              Password
+            </label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              placeholder='Username'
+              value={formValues.password}
+              onChange={handleInputChange}
+              className='border rounded w-full py-2 px-3 text-gray-700'
+            />
+          </div>
+          <button
+            type='submit'
+            className='bg-betway-green text-white py-2 w-52 block mx-auto hover:bg-green-700'
+          >
+            Login
+          </button>
+          <Link
+            href='/'
+            className='text-xs block text-center mt-2 text-betway-green'
+          >
+            Forgot password/username?
+          </Link>
+        </form>
       </div>
     </div>
   )
