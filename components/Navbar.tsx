88a0e5ae42afb6bafd5d3198ca615ce854722507
Murray Williams <murray@waterfalldigital.co.za>
2023-04-19 19:47:17 +0200

    Refactor out nav item colour variations

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,18 +2,16 @@ import Link from 'next/link'
 import { useState } from 'react'
 
 const navItems = [
-  { name: 'sports', href: '#', color: 'betway-green' },
-  { name: 'live & real', href: '#', color: 'betway-live' },
-  { name: 'casino', href: '#', color: 'betway-casino' },
-  { name: 'esports', href: '#', color: 'betway-esports' },
-  { name: 'vegas', href: '#', color: 'betway-vegas' },
+  { name: 'sports', href: '#' },
+  { name: 'live & real', href: '#' },
+  { name: 'casino', href: '#' },
+  { name: 'esports', href: '#' },
+  { name: 'vegas', href: '#' },
 ]
 
 const Navbar = () => {
   const [activeItem, setActiveItem] = useState(0)
-  const [itemColor, setItemColor] = useState(null)
 
-  console.log(activeItem)
   return (
     <nav className='relative z-10 bg-zinc-800'>
       <ul className='w-full grid grid-cols-5 text-center'>
@@ -25,7 +23,6 @@ const Navbar = () => {
             }`}
             onMouseEnter={() => setActiveItem(index)}
             onMouseLeave={() => setActiveItem(0)}
-            onClick={() => setItemColor(item.color)}
           >
             <Link
               href={item.href}
@@ -36,7 +33,7 @@ const Navbar = () => {
             <span
               className={`absolute bottom-0 left-0 w-full h-1 transition  ease-in-out duration-300 ${
                 index === activeItem ? 'scale-x-100' : 'scale-x-0'
-              } ${itemColor === item.color ? `bg-${item.color}` : ''}`}
+              } bg-betway-green`}
             ></span>
           </li>
         ))}
