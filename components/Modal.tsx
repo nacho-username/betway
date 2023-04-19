e2c59c5f795767c41aa1b5d3c428e39b510ae8a4
Murray Williams <murray@waterfalldigital.co.za>
2023-04-19 20:19:39 +0200

    Adds modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import { useState } from 'react'
+
+interface ModalProps {
+  isModalOpen: boolean
+}
+
+const Modal: React.FC<ModalProps> = ({ isModalOpen }) => {
+  const [activeForm, setActiveForm] = useState('login')
+
+  return (
+    <div
+      className={`fixed inset-0 flex items-center justify-center z-50 ${
+        isModalOpen ? 'block' : 'hidden'
+      }`}
+    >
+      <div className='bg-white rounded-lg p-8 w-full max-w-lg'>
+        <h2 className='text-2xl font-bold mb-4'>Login</h2>
+        <p className='mb-4 text-lg font-bold'>
+          New customer
+          <span className='underline text-betway-green'>Register here</span>
+        </p>
+      </div>
+    </div>
+  )
+}
+export default Modal
