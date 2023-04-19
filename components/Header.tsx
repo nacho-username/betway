e54026c99796c70dbb1e1c736502c47644e2fa00
Murray Williams <murray@waterfalldigital.co.za>
2023-04-19 10:10:53 +0200

    Updates header padding

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Header = () => {
   return (
     <header className='bg-black shadow w-full'>
-      <div className='max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8'>
+      <div className='max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8'>
         <div className='flex justify-between h-16'>
           <div className='flex-shrink-0 flex items-center'>
             <Image
