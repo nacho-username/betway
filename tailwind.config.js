0d853bf7bf98ed9914165dd9a2a5e0d51e300594
Murray Williams <murray@waterfalldigital.co.za>
2023-04-19 08:57:36 +0200

    Adds betway green color to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'betway-green': '#439539',
+      },
+    },
   },
   plugins: [],
 }
