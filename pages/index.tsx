f16aaf911662e00ad7f6d51e4fe9ca907b2e8d6d
Murray Williams <murray@waterfalldigital.co.za>
2023-04-19 14:25:35 +0200

    Adds background image to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,14 @@ import Layout from '../components/Layout'
 function Homepage() {
   return (
     <Layout>
-      <Image
-        className=''
-        src='/assets/the-hunch-mobile.webp'
-        alt='The Hutch Mobile'
-        width={1920}
-        height={1080}
-      />
+      <div className='w-full h-full'>
+        <Image
+          className='object-cover mt-24 z-0'
+          src='/assets/the-hunch-mobile.webp'
+          alt='The Hutch Mobile'
+          fill
+        />
+      </div>
     </Layout>
   )
 }
