411e12f90f95dbe29b5f91bfb10771313373cd9a
Murray Williams <murray@waterfalldigital.co.za>
2023-04-20 18:59:48 +0200

    Fetch data from strapi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,36 @@
 import Image from 'next/image'
 import Layout from '../components/Layout'
 import Offer from '../components/Offer'
+import { fetchHomepageData } from '../helper'
 
-function Homepage() {
+function Homepage({ homepageData }) {
+  const { Navigation, homepageOffer } =
+    homepageData.data.homepage.data.attributes
   return (
-    <Layout>
+    <Layout nav={Navigation}>
       <Image
         className='object-cover mt-24 z-0'
         src='/assets/the-hunch-mobile.webp'
         alt='The Hutch Mobile'
         fill
       />
-      <Offer />
+      <Offer offer={homepageOffer} />
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const { homepageData, error } = await fetchHomepageData()
+
+  if (error) {
+    return {
+      props: { error },
+    }
+  }
+
+  return {
+    props: { homepageData },
+  }
 }
 
 export default Homepage
