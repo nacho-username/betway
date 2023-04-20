242bc940510414cda57ce86bf700e25d69538227
Murray Williams <murray@waterfalldigital.co.za>
2023-04-20 19:00:29 +0200

    Update to use strapi data

--- components/Offer.tsx
+++ components/Offer.tsx
@@ -1,16 +1,16 @@
-const Offer = () => {
+const Offer = ({ offer }) => {
+  const { text, subText, btnText } = offer[0]
+
   return (
     <div className='fixed bottom-0 w-full'>
       <div className='flex flex-col items-center py-4 justify-start h-32 w-full bg-black opacity-80'>
         <span className='text-white text-light uppercase text-xs'>
-          Sports New Customer Offer
+          {subText}
         </span>
-        <h3 className='text-xl text-white font-bold'>
-          Get up to £10 in Free Bets
-        </h3>
+        <h3 className='text-xl text-white font-bold'>{text}</h3>
       </div>
       <button className='absolute bottom-0 left-[50%] -translate-x-[50%] z-10 bg-betway-green mb-4 py-2 w-64 text-md text-white hover:bg-white hover:text-betway-green'>
-        Join Now
+        {btnText}
       </button>
     </div>
   )
