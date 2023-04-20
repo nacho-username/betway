dddae9f9fc4c7a4439f403305b806113f534d734
Murray Williams <murray@waterfalldigital.co.za>
2023-04-20 19:42:48 +0200

    Update navbar to use prop methods

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,25 @@
 import Link from 'next/link'
-import { useState } from 'react'
 
-const Navbar = ({ nav }) => {
-  console.log(nav)
-  const [activeItem, setActiveItem] = useState(0)
+type NavigationItem = {
+  id: string
+  title: string
+  href: string
+  accentColor: string
+}
 
+const Navbar = ({ nav, handleNavClick, activeNavItem }) => {
   return (
     <nav className='relative z-10 bg-zinc-800'>
       <ul className='w-full grid grid-cols-5 text-center'>
-        {nav.map((item: any, index: number) => (
+        {nav.map((item: any) => (
           <li
             key={item.id}
             className={`relative py-4 cursor-pointer ${
-              index === activeItem ? 'text-white' : 'text-white opacity-80'
+              item.id === activeNavItem.id
+                ? 'text-white'
+                : 'text-white opacity-80'
             }`}
-            onMouseEnter={() => setActiveItem(index)}
-            onMouseLeave={() => setActiveItem(0)}
+            onClick={() => handleNavClick(item)}
           >
             <Link
               href={item.href}
@@ -25,8 +29,11 @@ const Navbar = ({ nav }) => {
             </Link>
             <span
               className={`absolute bottom-0 left-0 w-full h-1 transition  ease-in-out duration-300 ${
-                index === activeItem ? 'scale-x-100' : 'scale-x-0'
-              } bg-betway-green`}
+                activeNavItem.accentColor === item.accentColor
+                  ? `scale-x-100`
+                  : 'scale-x-0'
+              }`}
+              style={{ backgroundColor: `#${activeNavItem.accentColor}` }}
             ></span>
           </li>
         ))}
