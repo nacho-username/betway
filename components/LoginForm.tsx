2005446ea129a17e9e013261dc271b44350b2565
Murray Williams <murray@waterfalldigital.co.za>
2023-04-20 08:19:36 +0200

    Updates login form to come from component

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,4 +1,73 @@
+import Link from 'next/link'
+import { useState } from 'react'
+
+interface FormValues {
+  [key: string]: string
+}
+
 const LoginForm = () => {
-  return <div>LoginForm</div>
+  const [formValues, setFormValues] = useState<FormValues>({
+    email: '',
+    password: '',
+  })
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(formValues)
+  }
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <div className='mb-4'>
+          <label htmlFor='email' className='block text-gray-700 text-sm mb-2'>
+            Username
+          </label>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            placeholder='Username'
+            value={formValues.email}
+            onChange={handleInputChange}
+            className='border rounded w-full py-2 px-3 text-gray-700'
+          />
+        </div>
+        <div className='mb-4'>
+          <label
+            htmlFor='password'
+            className='block text-gray-700 text-sm mb-2'
+          >
+            Password
+          </label>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            placeholder='Username'
+            value={formValues.password}
+            onChange={handleInputChange}
+            className='border rounded w-full py-2 px-3 text-gray-700'
+          />
+        </div>
+        <button
+          type='submit'
+          className='bg-betway-green text-white py-2 w-52 block mx-auto hover:bg-green-700'
+        >
+          Login
+        </button>
+        <Link
+          href='/'
+          className='text-xs block text-center mt-2 text-betway-green'
+        >
+          Forgot password/username?
+        </Link>
+      </form>
+    </>
+  )
 }
 export default LoginForm
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,31 +1,14 @@
 import Link from 'next/link'
 import { useState } from 'react'
+import LoginForm from './LoginForm'
 
 interface ModalProps {
   handleCloseModal: () => void
   isModalOpen: boolean
 }
 
-interface FormValues {
-  [key: string]: string
-}
-
 const Modal: React.FC<ModalProps> = ({ isModalOpen, handleCloseModal }) => {
   const [activeForm, setActiveForm] = useState('login')
-  const [formValues, setFormValues] = useState<FormValues>({
-    email: '',
-    password: '',
-  })
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setFormValues({ ...formValues, [name]: value })
-  }
-
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    console.log(formValues)
-  }
 
   return (
     <div
@@ -45,51 +28,7 @@ const Modal: React.FC<ModalProps> = ({ isModalOpen, handleCloseModal }) => {
           New customer{' '}
           <span className='underline text-betway-green'>Register here</span>
         </p>
-        <form onSubmit={handleFormSubmit}>
-          <div className='mb-4'>
-            <label htmlFor='email' className='block text-gray-700 text-sm mb-2'>
-              Username
-            </label>
-            <input
-              type='email'
-              name='email'
-              id='email'
-              placeholder='Username'
-              value={formValues.email}
-              onChange={handleInputChange}
-              className='border rounded w-full py-2 px-3 text-gray-700'
-            />
-          </div>
-          <div className='mb-4'>
-            <label
-              htmlFor='password'
-              className='block text-gray-700 text-sm mb-2'
-            >
-              Password
-            </label>
-            <input
-              type='password'
-              name='password'
-              id='password'
-              placeholder='Username'
-              value={formValues.password}
-              onChange={handleInputChange}
-              className='border rounded w-full py-2 px-3 text-gray-700'
-            />
-          </div>
-          <button
-            type='submit'
-            className='bg-betway-green text-white py-2 w-52 block mx-auto hover:bg-green-700'
-          >
-            Login
-          </button>
-          <Link
-            href='/'
-            className='text-xs block text-center mt-2 text-betway-green'
-          >
-            Forgot password/username?
-          </Link>
-        </form>
+        <LoginForm />
       </div>
     </div>
   )
